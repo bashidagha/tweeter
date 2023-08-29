8614b039e0828633bddf9106751644d82e0bdc69
farshid karimi <karimi_f@yaftar.ir>
2023-08-29 15:59:04 +0430

    added unicons pkg

---
+++ frontend/src/app/profile/login/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { UilAirplay } from "@iconscout/react-unicons";
+
+export default function Login() {
+  return (
+    <div className={styles.main}>
+      asdas
+      <UilAirplay />
+    </div>
+  );
+}
